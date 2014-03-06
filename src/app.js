29f0f1fc48d803f14dda4c0468e49a5fbec5a5c8
tuvistavie <tuvistavie@gmail.com>
2014-03-06 16:12:44 +0900

    Start adding basic wrapper for Leap.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 var Leap = require('leapjs');
 var controller = new Leap.Controller();
 
-
 controller.on('frame', function(frame) {
   console.log(frame);
 });
---
+++ src/leap-manager.js
@@ -0,0 +1,61 @@
+var Leap = require('leapjs');
+var _ = require('underscore');
+
+var started = false;
+var controller = new Leap.Controller();
+
+var events = {};
+
+var options = {
+  host: '127.0.0.1',
+  port: 6437,
+  enableGestures: true,
+  frameEventName: 'deviceFrame',
+  useAllplugins: false
+};
+
+var LeapManager = function(overrides) {
+  _.extend(options, overrides);
+};
+
+_.extend(LeapManager.prototype, {
+  start: function() {
+    if (started) {
+      return;
+    }
+    controller.connect();
+    started = true;
+  },
+
+  stop: function() {
+    if (!started) {
+      return;
+    }
+    controller.disconnect();
+    started = false;
+  },
+
+  on: function(eventName, callback) {
+    events[eventName] = callback;
+  },
+
+  off: function(eventName) {
+    if (eventName) {
+      delete events[eventName];
+    } else {
+      // remove all events when called without arguments
+      events = {};
+    }
+  },
+
+  trigger: function(eventName) {
+    var callback = events[eventName];
+    if (!callback) {
+      return;
+    }
+    var arguments = Array.prototype.slice.call(arguments, 1);
+    callback(arguments);
+  }
+};
+
+module.exports = LeapManager;
