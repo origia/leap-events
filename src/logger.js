dde5a4a9964f6e172982f9be0c3c7439d9442ef4
tuvistavie <tuvistavie@gmail.com>
2014-03-07 18:06:05 +0900

    Add logger.

---
+++ src/logger.js
@@ -0,0 +1,17 @@
+/**
+ * Wrapper for winston
+**/
+
+"use strict";
+
+var winston = require('winston')
+
+var logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      level: 'debug'
+    })
+  ]
+})
+
+module.exports = logger
