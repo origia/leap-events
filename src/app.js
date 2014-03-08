0a82a5aeb849e2e0ee02cab2dd097622619e9618
Daniel Perez <tuvistavie@gmail.com>
2014-03-08 15:01:29 +0900

    Update main file.

--- src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var LeapManager = require('./leap-manager')
-
-var manager = new LeapManager()
-manager.on('oneFingerMove', function (state) {
-  console.log(state.screenPosition())
-})
-manager.start()
---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = { LeapManager: require('./leap-manager')
+                 , FrameState: require('./frame-state')
+                 }
